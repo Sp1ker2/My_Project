968110a2ae5ab859631fa5582a670c66e892065d
Invalidsyntaxisbot <101515672+Sp1ker2@users.noreply.github.com>
2024-02-20 09:27:20 +0200

    Add files via upload

--- JWT_AUTH/core/config.py
+++ JWT_AUTH/core/config.py
@@ -3,17 +3,22 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
-DB_PASS = BASE_DIR / "db.sqlite3"
+
+DB_PATH = BASE_DIR / "db.sqlite3"
 
 
 class DbSettings(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{DB_PASS}"
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    # echo: bool = False
     echo: bool = True
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
+
     db: DbSettings = DbSettings()
+
+    # db_echo: bool = True
 
 
 settings = Settings()
