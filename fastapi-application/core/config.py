13302d09c5cca2e6de34744745b5899e7d64b48d
Sp1ker2 <bogdan.speaker@gmail.com>
2025-04-22 09:37:18 +0300

    configure config.py

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -13,6 +13,15 @@ class DatabaseConfig(BaseModel):
     echo_pool : bool = False
     max_overflow : int = 10
     pool_size : int = 50
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template",".env"),
@@ -29,4 +38,3 @@ class Settings(BaseSettings):
     db: DatabaseConfig
 
 settings = Settings()
-print(settings.db.url)
