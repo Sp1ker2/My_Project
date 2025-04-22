f25bdbe6c73857bbb3493b36766d03af414621d0
Sp1ker2 <bogdan.speaker@gmail.com>
2025-04-22 14:49:14 +0300

    Add IntIdPkMixin for reusable primary key definition
    Created a mixin to define a primary key column for models, improving code reuse and consistency. Updated the User model to inherit the primary key from the new mixin.

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-
+from .mixins.int_id_pk import IntIdPkMixin as ipkm
 from .base import Base
 class User(Base):
+    ipkm.id
     username: Mapped[str] = mapped_column(unique=True)
     foo:Mapped[int]
     bar:Mapped[int]
